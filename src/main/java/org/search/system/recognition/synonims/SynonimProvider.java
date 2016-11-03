ba6fb71ac3df88d76228b7a056113c1c8cc03faa
matkovd <daniil.matkov@gmail.com>
2016-11-03 20:16:29 +0300

    recognizer prototype

--- src/main/java/org/search/system/recognition/synonims/SynonimProvider.java
+++ src/main/java/org/search/system/recognition/synonims/SynonimProvider.java
@@ -4,9 +4,7 @@ import org.search.system.dao.WordDao;
 
 import java.util.List;
 
-/**
- * Created by Daniil Matkov on 03.11.16.
- */
+
 public class SynonimProvider {
     public List<String> getSynonymsFromDB(String word) {
         WordDao wordDao = new WordDao();
--- src/test/java/org/search/system/recognition/synonims/SynonimProviderTest.java
+++ src/test/java/org/search/system/recognition/synonims/SynonimProviderTest.java
@@ -2,9 +2,6 @@ package org.search.system.recognition.synonims;
 
 import junit.framework.TestCase;
 
-/**
- * Created by Daniil Matkov on 03.11.16.
- */
 public class SynonimProviderTest extends TestCase {
     public void testGetSynonymsFromDB() throws Exception {
 
