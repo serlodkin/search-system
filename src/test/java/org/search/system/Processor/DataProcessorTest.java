0e5aba110ff1c823fb2466fb748d254a94cf3f80
matkovd <daniil.matkov@gmail.com>
2016-11-04 18:57:53 +0300

    add tests

--- src/test/java/org/search/system/Processor/DataProcessorTest.java
+++ src/test/java/org/search/system/Processor/DataProcessorTest.java
@@ -30,17 +30,52 @@ public class DataProcessorTest extends TestCase {
         test.put("Data", first);
         test.put("Test", second);
         ArrayList<Word> d = new ArrayList<>();
-        d.add(new Word("Data", new ArrayList<String>()));
-        d.add(new Word("Test", new ArrayList<String>()));
+        d.add(new Word("Data", new ArrayList<>()));
+        d.add(new Word("Test", new ArrayList<>()));
         ArrayList<Page> result = dataProcessor.process(test, d);
-
         assertEquals(result.size(), 1);
         assertEquals(result.get(0).getTitle(), "Test");
         assertEquals(result.get(0).getRang(), 0);
         assertEquals(result.get(0).getDescription(), "Test");
         assertEquals(result.get(0).getLink(), "http://example.com");
+    }
 
+    public void testProcess1() throws Exception {
+        DataProcessor dataProcessor = new DataProcessor();
+        HashMap<String, HashSet<Page>> test = new HashMap<>();
+        HashSet<Page> first = new HashSet<>();
+        ArrayList<String> synonims = new ArrayList<>();
+        synonims.add("statistics");
+        synonims.add("documents");
+        Page t = new Page("Test", "Test", synonims, "http://example.com", 0);
+        Page t1 = new Page("Test", "Data", synonims, "http://example.com", 0);
+        Page t2 = new Page("Data", "Test", synonims, "http://example.com", 0);
+        first.add(t1);
+        first.add(t2);
+        test.put("Data", first);
+        ArrayList<Word> d = new ArrayList<>();
+        d.add(new Word("Data", new ArrayList<>()));
+        ArrayList<Page> result = dataProcessor.process(test, d);
+        assertEquals(result.size(), 2);
 
+        assertEquals(result.get(0).getLink(), "http://example.com");
+        assertEquals(result.get(0).getTitle(), "Data");
+        assertEquals(result.get(0).getDescription(), "Test");
+        assertEquals(result.get(0).getRang(), 0);
+
+        assertEquals(result.get(1).getLink(), "http://example.com");
+        assertEquals(result.get(1).getTitle(), "Test");
+        assertEquals(result.get(1).getDescription(), "Data");
+        assertEquals(result.get(1).getRang(), 0);
+
+    }
+
+    public void testProcess2() throws Exception {
+        DataProcessor dataProcessor = new DataProcessor();
+        HashMap<String, HashSet<Page>> test = new HashMap<>();
+        ArrayList<Word> d = new ArrayList<>();
+        ArrayList<Page> result = dataProcessor.process(test, d);
+        assertEquals(result.size(), 0);
     }
 
 }
