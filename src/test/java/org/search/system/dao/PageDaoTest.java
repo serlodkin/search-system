b862af3d2df46240406da4baffa8555a67764338
matkovd <daniil.matkov@gmail.com>
2016-11-04 14:57:15 +0300

    dao tests

---
+++ src/test/java/org/search/system/dao/PageDaoTest.java
@@ -0,0 +1,41 @@
+package org.search.system.dao;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.search.system.models.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageDaoTest extends TestCase {
+    @Before
+    public void init() {
+        PageDao pageDao = new PageDao();
+        ArrayList<String> test = new ArrayList<>();
+        test.add("test");
+        test.add("tests");
+        test.add("testing");
+        pageDao.insert(new Page("Test", "test test", test, "http://example.com", 0));
+    }
+
+    public void testInsert() throws Exception {
+        PageDao pageDao = new PageDao();
+        ArrayList<String> test = new ArrayList<>();
+        test.add("data");
+        test.add("Data");
+        pageDao.insert(new Page("Data", "data", test, "http://example.com", 0));
+
+    }
+
+    public void testGetPages() throws Exception {
+        PageDao pageDao = new PageDao();
+        ArrayList<String> excepted = new ArrayList<>();
+        excepted.add("test");
+        excepted.add("tests");
+        excepted.add("testing");
+
+        List<Page> result = pageDao.getPages("test");
+        assertEquals(excepted, result.get(0).getTags());
+    }
+
+}
--- src/test/java/org/search/system/recognition/TagsRecognizerTest.java
+++ src/test/java/org/search/system/recognition/TagsRecognizerTest.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 public class TagsRecognizerTest extends TestCase {
     public void testRecognize() throws Exception {
 
+        TagsRecognizer t = new TagsRecognizer();
+        System.out.print(t.recognize("data"));
     }
 
 }
