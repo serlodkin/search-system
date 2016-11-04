0130513153b4c6e819d5be009de806b34f6e5ae8
matkovd <daniil.matkov@gmail.com>
2016-11-04 16:50:50 +0300

    action updated

--- src/main/java/org/search/system/actions/ResultAction.java
+++ src/main/java/org/search/system/actions/ResultAction.java
@@ -1,10 +1,14 @@
 package org.search.system.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.search.system.dao.PageDao;
+import org.search.system.models.Page;
 import org.search.system.models.Word;
 import org.search.system.recognition.TagsRecognizer;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.WeakHashMap;
 
 public class ResultAction extends ActionSupport{
     private String searchQuery;
@@ -21,8 +25,17 @@ public class ResultAction extends ActionSupport{
     public String execute(){
         TagsRecognizer recognizer = new TagsRecognizer();
         ArrayList<Word> tags = recognizer.recognize(searchQuery);
-        //for(String tag:)
-
+        WeakHashMap<String, List<Page>> resultByTag = new WeakHashMap<>();
+        PageDao pageDao = new PageDao();
+        ArrayList<Page> temp = new ArrayList<>();
+        for (Word tag : tags) {
+            temp.addAll(pageDao.getPages(tag.getWord()));
+            for (String synonim : tag.getSynonims()) {
+                temp.addAll(pageDao.getPages(tag.getWord()));
+            }
+            resultByTag.put(tag.getWord(), temp);
+            temp.clear();
+        }
         return SUCCESS;
     }
 
