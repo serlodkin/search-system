731871a2c98798085af0940ae58e0b9e832c52c3
matkovd <daniil.matkov@gmail.com>
2016-11-04 15:32:13 +0300

    new tests added

--- src/test/java/org/search/system/dao/WordDaoTest.java
+++ src/test/java/org/search/system/dao/WordDaoTest.java
@@ -2,18 +2,41 @@ package org.search.system.dao;
 
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.search.system.models.Word;
+
+import java.util.ArrayList;
 
 /**
  * Created by Daniil Matkov on 04.11.16.
  */
 public class WordDaoTest extends TestCase {
-    @Before
-    public void init() {
-
+    public void testInsert() throws Exception {
+        WordDao wordDao = new WordDao();
+        ArrayList<String> synonims = new ArrayList<>();
+        synonims.add("testing");
+        synonims.add("tests");
+        wordDao.insert(new Word("test", synonims));
     }
 
-    public void testGetWord() throws Exception {
+    @Before
+    public void init() {
+        WordDao wordDao = new WordDao();
+        ArrayList<String> synonims = new ArrayList<>();
+        synonims.add("statistics");
+        synonims.add("documents");
+        wordDao.insert(new Word("data", synonims));
+    }
 
+
+    public void testGetWord() throws Exception {
+        WordDao wordDao = new WordDao();
+        ArrayList<String> synonims = new ArrayList<>();
+        synonims.add("statistics");
+        synonims.add("documents");
+        Word excepted = new Word("data", synonims);
+        Word result = wordDao.getWord("data");
+        assertEquals(excepted.getWord(), result.getWord());
+        assertEquals(excepted.getSynonims(), result.getSynonims());
     }
 
 }
