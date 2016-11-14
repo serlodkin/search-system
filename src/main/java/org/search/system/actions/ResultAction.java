98c9120455a3f77aaaafa08e7d899b1aca1985f2
Daniil Matkov <daniil.matkov@gmail.com>
2016-11-14 18:10:05 +0300

    caching added

--- src/main/java/org/search/system/actions/ResultAction.java
+++ src/main/java/org/search/system/actions/ResultAction.java
@@ -1,15 +1,11 @@
 package org.search.system.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
-import org.search.system.dao.PageDao;
+import org.search.system.cached.ResultCache;
 import org.search.system.models.Page;
-import org.search.system.models.Word;
-import org.search.system.processor.DataProcessor;
-import org.search.system.recognition.TagsRecognizer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
 
 public class ResultAction extends ActionSupport{
     private String searchQuery;
@@ -24,23 +20,13 @@ public class ResultAction extends ActionSupport{
 
     @Override
     public String execute() {
-        TagsRecognizer recognizer = new TagsRecognizer();
-        ArrayList<Word> tags = recognizer.recognize(searchQuery);
-        HashMap<String, HashSet<Page>> resultByTag = new HashMap<>();
-        PageDao pageDao = new PageDao();
-        HashSet<Page> temp = new HashSet<>();
-        for (Word tag : tags) {
-            temp.addAll(pageDao.getPages(tag.getWord()));
-
-            for (String synonim : tag.getSynonims()) {
-                temp.addAll(pageDao.getPages(synonim));
-            }
-            resultByTag.put(tag.getWord(), (HashSet<Page>) temp.clone());
-            temp.clear();
+        result = new ArrayList<>();
+        ResultCache cache = new ResultCache();
+        try {
+            result = cache.resultsCache.get(searchQuery);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-
-        DataProcessor dataProcessor = new DataProcessor();
-        result = dataProcessor.process(resultByTag, tags);
 
         return SUCCESS;
     }
