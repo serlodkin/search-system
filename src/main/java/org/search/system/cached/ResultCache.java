6a69a20b0858d5d332d9593c2c5263ced3579548
Daniil Matkov <daniil.matkov@gmail.com>
2016-11-14 17:19:43 +0300

    caching added

---
+++ src/main/java/org/search/system/cached/ResultCache.java
@@ -0,0 +1,50 @@
+package org.search.system.cached;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.search.system.dao.PageDao;
+import org.search.system.models.Page;
+import org.search.system.models.Word;
+import org.search.system.processor.DataProcessor;
+import org.search.system.recognition.TagsRecognizer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Daniil on 14.11.2016.
+ */
+public class ResultCache {
+    public LoadingCache<String, ArrayList<Page>> resultsCache;
+
+    {
+        resultsCache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterAccess(10, TimeUnit.HOURS)
+                .build(new CacheLoader<String, ArrayList<Page>>() {
+                    @Override
+                    public ArrayList<Page> load(String searchQuery) throws Exception {
+                        TagsRecognizer recognizer = new TagsRecognizer();
+                        ArrayList<Word> tags = recognizer.recognize(searchQuery);
+                        HashMap<String, HashSet<Page>> resultByTag = new HashMap<>();
+                        PageDao pageDao = new PageDao();
+                        HashSet<Page> temp = new HashSet<>();
+                        for (Word tag : tags) {
+                            temp.addAll(pageDao.getPages(tag.getWord()));
+
+                            for (String synonim : tag.getSynonims()) {
+                                temp.addAll(pageDao.getPages(synonim));
+                            }
+                            resultByTag.put(tag.getWord(), (HashSet<Page>) temp.clone());
+                            temp.clear();
+                        }
+
+                        DataProcessor dataProcessor = new DataProcessor();
+                        return dataProcessor.process(resultByTag, tags);
+                    }
+                });
+    }
+}
