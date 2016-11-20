21ab1fa843d957910921b1a0022d9f53e81b326a
Daniil Matkov <daniil.matkov@gmail.com>
2016-11-20 10:35:22 +0300

    fix

--- src/main/java/org/search/system/calc/RangCalc.java
+++ src/main/java/org/search/system/calc/RangCalc.java
@@ -1,7 +1,13 @@
 package org.search.system.calc;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by Daniil on 13.11.2016.
  */
 public class RangCalc {
+    public long calc(String keyword,String description){
+        long count=StringUtils.countMatches(keyword,description);
+        return count;
+    }
 }
