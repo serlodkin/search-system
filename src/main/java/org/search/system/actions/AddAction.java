bd0a26ed782358dfa12453a154e03cc93ace31ad
Daniil Matkov <daniil.matkov@gmail.com>
2016-12-24 18:00:37 +0300

    cookies added

--- src/main/java/org/search/system/actions/AddAction.java
+++ src/main/java/org/search/system/actions/AddAction.java
@@ -1,15 +1,23 @@
 package org.search.system.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 import org.search.system.dao.PageDao;
 import org.search.system.models.Page;
 import org.search.system.parsers.HtmlParser;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Daniil on 10.11.2016.
  */
-public class AddAction extends ActionSupport {
+public class AddAction extends ActionSupport implements ServletResponseAware, ServletRequestAware {
     private String url;
+    private HttpServletResponse servletResponse;
+    private HttpServletRequest servletRequest;
 
     @Override
     public String execute() {
@@ -17,6 +25,19 @@ public class AddAction extends ActionSupport {
         Page result = parser.parse(url);
         PageDao pageDao = new PageDao();
         pageDao.insert(result);
+        Cookie history = null;
+        for (Cookie cookie : servletRequest.getCookies()) {
+            if (cookie.getName().equals("history")) {
+                history = cookie;
+            }
+
+        }
+        if (history == null) {
+            history = new Cookie("history", "");
+        }
+        history.setValue(history.getValue() + '|' + url);
+        servletResponse.addCookie(history);
+
         return SUCCESS;
     }
 
@@ -26,5 +47,15 @@ public class AddAction extends ActionSupport {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public void setServletRequest(HttpServletRequest httpServletRequest) {
+        this.servletResponse = servletResponse;
+    }
+
+    @Override
+    public void setServletResponse(HttpServletResponse httpServletResponse) {
+        this.servletRequest = servletRequest;
     }
 }
