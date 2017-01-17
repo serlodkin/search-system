83c0011a68fc72e87ea96c9146b2637eaa30bad8
Daniil Matkov <daniil.matkov@gmail.com>
2017-01-17 20:07:40 +0300

    Add Concurrent user class.

---
+++ src/main/java/org/search/system/concurrent/ConcurrentUser.java
@@ -0,0 +1,86 @@
+/*
+MIT License
+
+Copyright (c) 2016-2017 Daniil Matkov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package org.search.system.concurrent;
+
+import org.search.system.models.User;
+
+import java.util.ArrayList;
+
+/*
+ * Thread-safe user decorator
+ * @author Daniil Matkov
+ */
+public class ConcurrentUser {
+
+    private final User origin;
+
+    public ConcurrentUser(User origin) {
+        this.origin = origin;
+    }
+
+    public synchronized String getAddress() {
+        return this.origin.getAddress();
+    }
+
+    public synchronized String getCountry() {
+        return this.origin.getCountry();
+    }
+
+    public synchronized void setCountry(String country) {
+        this.origin.setCountry(country);
+    }
+
+    public synchronized String getLanguage() {
+        return this.origin.getLanguage();
+    }
+
+    public synchronized ArrayList<String> getHistory() {
+        return this.origin.getHistory();
+    }
+
+    public synchronized void addToHistory(String request) {
+        this.origin.addToHistory(request);
+    }
+
+    public synchronized void clearHistory() {
+        this.origin.clearHistory();
+    }
+
+    public synchronized void deleteHistory(String toDelete) {
+        this.origin.deleteHistory(toDelete);
+    }
+
+    public synchronized void deleteHistory(int toDelete) {
+        this.origin.deleteHistory(toDelete);
+    }
+
+    public synchronized String getId() {
+        return this.origin.getId();
+    }
+
+    public synchronized void setId(String id) {
+        this.origin.setId(id);
+    }
+
+}
---
+++ src/test/java/org/search/system/concurrent/ConcurrentUserTest.java
@@ -0,0 +1,87 @@
+package org.search.system.concurrent;
+
+import junit.framework.TestCase;
+import org.search.system.models.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Daniil on 17.01.2017.
+ */
+public class ConcurrentUserTest extends TestCase {
+    public void testGetAddress() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        assertEquals("Some address", user.getAddress());
+    }
+
+    public void testGetCountry() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        assertEquals("Some country", user.getCountry());
+    }
+
+    public void testSetCountry() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.setCountry("Another country");
+        assertEquals("Another country", user.getCountry());
+    }
+
+    public void testGetLanguage() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        assertEquals("Some language", user.getLanguage());
+    }
+
+    public void testGetHistory() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        assertEquals(new ArrayList<String>(), user.getHistory());
+    }
+
+    public void testAddToHistory() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.addToHistory("test");
+        ArrayList<String> excepted = new ArrayList<>();
+        excepted.add("test");
+        assertEquals(excepted, user.getHistory());
+    }
+
+    public void testClearHistory() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.addToHistory("test");
+        user.clearHistory();
+        assertEquals(new ArrayList<String>(), user.getHistory());
+    }
+
+    public void testDeleteHistory() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.addToHistory("test");
+        user.addToHistory("data");
+        user.deleteHistory(0);
+        ArrayList<String> excepted = new ArrayList<>();
+        excepted.add("data");
+        assertEquals(excepted, user.getHistory());
+    }
+
+    public void testDeleteHistory1() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.addToHistory("test");
+        user.addToHistory("data");
+        user.deleteHistory("data");
+        ArrayList<String> excepted = new ArrayList<>();
+        excepted.add("test");
+        assertEquals(excepted, user.getHistory());
+    }
+
+    public void testGetId() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.setId("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+        assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709", user.getId());
+    }
+
+    public void testSetId() throws Exception {
+        ConcurrentUser user = new ConcurrentUser(new User("Some address", "Some country", "Some language"));
+        user.setId("da39a3ee5e6b4b0d3255bfef95601890afd80709");
+        assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709", user.getId());
+        user.setId("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", user.getId());
+    }
+
+}
